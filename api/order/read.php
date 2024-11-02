b142832f0d3204febbf11d5a0a14418862bca73d
phihung0131 <phihung0131@gmail.com>
2024-11-02 22:23:06 +0700

    .

--- api/order/read.php
+++ api/order/read.php
@@ -1,6 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
+header('Access-Control-Allow-Headers: Authorization, Content-Type');
 
 include_once '../../config/db.php';
 include_once '../../model/order.php';
