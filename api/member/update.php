65963900415c05240f3b99fc9bb91e6f08b41045
phihung0131 <phihung0131@gmail.com>
2024-11-02 22:42:09 +0700

    May loi vat vat

--- api/member/update.php
+++ api/member/update.php
@@ -1,9 +1,8 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, 
-Access-Control-Allow-Methods, Authorization, X-Requested-With');
+header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 include_once '../../config/db.php';
 include_once '../../model/member.php';
 include_once '../../middleware/middleware.php';
